c92a178f88a621bfc2c4b1a9a0961f16999bb016
Demian Calderon <acxel.demian.calderon.godinez@gmail.coom>
2024-05-31 21:33:41 -0600

    31-05-2024

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/idyhome', [App\Http\Controllers\IDYHomeController::class, 'index'])->name('idyhome');
 Route::get('/pacientes', [App\Http\Controllers\PacientesController::class, 'index'])->name('pacientes');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+Route::get('/bitacora', [App\Http\Controllers\BitacoraController::class, 'index'])->name('bitacora');
+Route::get('/reportes', [App\Http\Controllers\ReportesController::class, 'index'])->name('reportes');
