8725c9e5e7402b2341e5c05c061dd5ca18cd04ce
Demian <acxel.demian.calderon.godinez@gmail.com>
2024-06-24 20:03:32 -0600

    24/06/2024 V20:03 Demian

--- resources/views/MPacientes/pacientes.blade.php
+++ resources/views/MPacientes/pacientes.blade.php
@@ -115,17 +115,7 @@
             </div>
         </div>
     </div>
-
-    <!-- Cuadro de diálogo personalizado -->
-    <div id="customDialog">
-        <div class="dialogBox">
-            <p>¿Estás seguro de que deseas eliminar este paciente?</p>
-            <button class="confirmBtn" id="confirmDelete">Sí</button>
-            <button class="cancelBtn" id="cancelDelete">No</button>
-        </div>
-    </div>
-
-    <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script>
