7a8720ee0513239d50ea6a42a9d419daba85665d
BetoPlox <betololplox@gmail.com>
2024-07-02 20:33:29 -0600

    02/07 V-20:33 BETO Aplicacion de loader en Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,10 @@
 </head>
 <body>
     <main>
+        <div id="loader-wrapper">
+            <span class="loader"></span>
+        </div>
+
         <div class="grid-container">
             <a href="{{ route('appointments.index')}}" class="grid-item">
                 <img src="EstilosHome/imagenes/citas.png" alt="Módulo de Citas" class="img">
@@ -38,6 +42,9 @@
             </a>
         </div>
     </main>
+
+    <script src="EstilosHome/script.js"></script>
+
 </body>
 </html>
 @endsection
