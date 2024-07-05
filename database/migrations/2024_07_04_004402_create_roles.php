b8cd795db1937bf9ae5912c107f24dc3862df3e1
BetoPlox <betololplox@gmail.com>
2024-07-04 20:08:27 -0600

    04-07 V-20:08 BETO

--- database/migrations/2024_07_04_004402_create_roles.php
+++ database/migrations/2024_07_04_004402_create_roles.php
@@ -13,11 +13,35 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $role1 = Role::create(['name' => 'admin']);
-        $role2 = Role::create(['name' => 'empleado']);
-        $role3 = Role::create(['name' => 'visor']);
+        // Verificar si el rol 'admin' ya existe
+        if (!Role::where('name', 'admin')->exists()) {
+            $role1 = Role::create(['name' => 'admin']);
+        } else {
+            $role1 = Role::where('name', 'admin')->first();
+        }
+
+        // Verificar si el rol 'empleado' ya existe
+        if (!Role::where('name', 'empleado')->exists()) {
+            $role2 = Role::create(['name' => 'empleado']);
+        } else {
+            $role2 = Role::where('name', 'empleado')->first();
+        }
+
+        // Verificar si el rol 'visor' ya existe
+        if (!Role::where('name', 'visor')->exists()) {
+            $role3 = Role::create(['name' => 'visor']);
+        } else {
+            $role3 = Role::where('name', 'visor')->first();
+        }
+
+        // Asignar el rol al usuario con ID 1
         $user = User::find(1);
-        $user->assignRole($role1);  // Corrige el método assiingRole a assignRole
+        if ($user) {
+            $user->assignRole($role1);
+        } else {
+            // Manejar el caso en que el usuario no exista
+            echo "Usuario con ID 1 no encontrado.";
+        }
     }
 
     /**
