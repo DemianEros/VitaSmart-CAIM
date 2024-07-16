87318dc587ba2bc883c6420e7f7f6e2d1e971218
Demian <acxel.demian.calderon.godinez@gmail.com>
2024-07-15 21:19:55 -0600

    15/07/2024mV 21:19 Demian

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
@@ -14,25 +13,26 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $role1 = Role::create(['name' => 'Admin']);
-        $role2 = Role::create(['name' => 'Administrativo']);
-        $role3 = Role::create(['name' => 'Visor']);
+        // Verificar si los roles ya existen antes de crearlos
+        $role1 = Role::firstOrCreate(['name' => 'Admin']);
+        $role2 = Role::firstOrCreate(['name' => 'Administrativo']);
+        $role3 = Role::firstOrCreate(['name' => 'Visor']);
 
-        //Permisos para el modulo de Citas
-        Permission::create(['name' => 'appointments.index'])->syncRoles([$role1, $role2, $role3]);
-        Permission::create(['name' => 'appointments.create'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'appointments.store'])->syncRoles([$role1, $role2, $role3]);
-        Permission::create(['name' => 'appointments.show'])->syncRoles([$role1, $role2, $role3]);
-        Permission::create(['name' => 'appointments.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'appointments.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'appointments.destroy'])->syncRoles([$role1, $role2]);
+        // Permisos para el módulo de Citas
+        Permission::firstOrCreate(['name' => 'appointments.index'])->syncRoles([$role1, $role2, $role3]);
+        Permission::firstOrCreate(['name' => 'appointments.create'])->syncRoles([$role1, $role2]);
+        Permission::firstOrCreate(['name' => 'appointments.store'])->syncRoles([$role1, $role2, $role3]);
+        Permission::firstOrCreate(['name' => 'appointments.show'])->syncRoles([$role1, $role2, $role3]);
+        Permission::firstOrCreate(['name' => 'appointments.edit'])->syncRoles([$role1, $role2]);
+        Permission::firstOrCreate(['name' => 'appointments.update'])->syncRoles([$role1, $role2]);
+        Permission::firstOrCreate(['name' => 'appointments.destroy'])->syncRoles([$role1, $role2]);
 
-        //Permisos para el modulo de Pacientes
-        Permission::create(['name' => 'pacientes'])->syncRoles([$role1, $role2, $role3]);
-        Permission::create(['name' => 'pacientes.create'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'pacientes.store'])->syncRoles([$role1, $role2, $role3]);
-        Permission::create(['name' => 'pacientes.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'pacientes.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'pacientes.destroy'])->syncRoles([$role1, $role2]);
+        // Permisos para el módulo de Pacientes
+        Permission::firstOrCreate(['name' => 'pacientes'])->syncRoles([$role1, $role2, $role3]);
+        Permission::firstOrCreate(['name' => 'pacientes.create'])->syncRoles([$role1, $role2]);
+        Permission::firstOrCreate(['name' => 'pacientes.store'])->syncRoles([$role1, $role2, $role3]);
+        Permission::firstOrCreate(['name' => 'pacientes.edit'])->syncRoles([$role1, $role2]);
+        Permission::firstOrCreate(['name' => 'pacientes.update'])->syncRoles([$role1, $role2]);
+        Permission::firstOrCreate(['name' => 'pacientes.destroy'])->syncRoles([$role1, $role2]);
     }
 }
