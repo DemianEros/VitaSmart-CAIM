00e85911f89c5537882b65811f4dd16a938e7148
Demian <acxel.demian.calderon.godinez@gmail.com>
2024-07-27 17:52:57 -0600

    Vistas por roles de usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,8 @@
             <span class="loader"></span>
         </div>
 
+    
+
         <div class="grid-container">
             <a href="{{ route('appointments.index')}}" class="grid-item">
                 <img src="EstilosHome/imagenes/citas.png" alt="Módulo de Citas" class="img">
@@ -26,14 +28,18 @@
                 <i class="fa-solid fa-calendar-days" style="color: #63E6BE;"></i>
                 <h2>Módulo de Pacientes</h2>
             </a>
+            @role('Admin')
             <a href="{{ route('admin') }}" class="grid-item">
                 <img src="EstilosHome/imagenes/Admin.png" alt="Módulo de Administrador" class="img">
                 <h2>Módulo de Administrador</h2>
             </a>
+            @endrole
+            @role('Admin|Administrativo')
             <a href="{{ route('bitacora') }}" class="grid-item">
                 <img src="EstilosHome/imagenes/Bitacora.png" alt="Bitácora de Expedientes" class="img">
                 <h2>Bitácora de Expedientes</h2>
             </a>
+            @endrole
             <a href="{{ url('/contacto') }}" target="_blank" class="grid-item">
                 <img src="EstilosHome/imagenes/conocenos.png" alt="Conócenos" class="img">
                 <h2>Conócenos</h2>
